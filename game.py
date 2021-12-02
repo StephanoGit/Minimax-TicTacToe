9b0a70d4b17a1e2df60cc12fd3cf6347c27fa770
Stephanno <poporaduu@gmail.com>
2021-12-02 18:21:09 +0000

    fixed minimax function

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-from tkinter import PhotoImage, Tk, Button, messagebox
-from typing import Counter
+from tkinter import PhotoImage, Tk, Button
 
 def create_bttns():
     for i in range(9):
-        squares[i] = Button(window, image=available_space, width="100", height="100",
-                            command=lambda value=i:handle_button_click(value))
+        squares[i] = Button(window,image=available_space, width="145", height="145",
+                            command=lambda value=i:handle_button_click(value),
+                            borderwidth=0, highlightthickness=0, highlightcolor="#5A3392")
 
 def checkdraw():
     for i in range(3):
@@ -15,18 +15,14 @@ def checkdraw():
 
 def checkwin():
     if (board[0][0] == board[1][1] == board[2][2] and board[0][0] != " "):
-        window.title("WIN")
         return True
     if (board[0][2] == board[1][1] == board[2][0] and board[0][2] != " "):
-        window.title("WIN")
         return True
 
     for i in range(3):
         if (board[i][0] == board[i][1] == board[i][2] and board[i][0] != " "):
-            window.title("WIN")
             return True
         if (board[0][i] == board[1][i] == board[2][i] and board[0][i] != " "):
-            window.title("WIN")
             return True
 
     return False
@@ -46,13 +42,13 @@ def checkwin2(symbol):
     return False
 
 def update_move(square_number, symbol):
-    square_to_board_map = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+    square_to_board_map = [[0, 0], [0, 1], [0, 2],
+                           [1, 0], [1, 1], [1, 2],
+                           [2, 0], [2, 1], [2, 2]]
     m = square_to_board_map
     s = square_number
     board[m[s][0]][m[s][1]] = symbol
 
-    print(s)
-    print(board)
     checkwin()
     checkdraw()
 
@@ -94,42 +90,44 @@ def minimax(board, depth, isMaximizing):
 
 def checkBestMove():
     bestScore  = -1000
-    move = [0, 0]
+    moveX = 0
+    moveY = 0
 
     for i in range(3):
         for j in range(3):
             if board[i][j] == " ":
                 board[i][j] = bot
-                score = minimax(board, 0, False)
-                board[i][j] = " "
+                score = minimax(board, 2, False)
                 if score > bestScore:
                     bestScore = score
-                    move[0] = j
-                    move[1] = i
+                    moveX = i
+                    moveY = j
+                board[i][j] = " "
     
-    squares[(move[0] * 3) + move[1]].configure(image=player2_taken)
-    update_move((move[0] * 3) + move[1], bot)
+    squares[(moveX * 3) + moveY].configure(image=player2_taken)
+    update_move((moveX * 3) + moveY, bot)
 
 
 def handle_button_click(button_number):
 
-    # Do player move
-    squares[button_number].configure(image=player1_taken)
-    update_move(button_number, player)
+    if checkwin() == False:
+        # Do player move
+        squares[button_number].configure(image=player1_taken)
+        update_move(button_number, player)
 
-    # Do bot move
-    checkBestMove()
+        # Do bot move
+        checkBestMove()
 
 
 window = Tk()
 
 window.title("OXO Game")
-window.geometry("300x300")
+window.geometry("435x435")
+window.configure(background="#5A3392")
 
-available_space = PhotoImage(file="images/myButton.png")
-player1_taken = PhotoImage(file="images/myButtonP1.png")
-player2_taken = PhotoImage(file="images/myButtonP2.png")
-winner = PhotoImage(file="images/winner.png")
+available_space = PhotoImage(file="images/empty.png")
+player1_taken = PhotoImage(file="images/player1.png")
+player2_taken = PhotoImage(file="images/player2.png")
 
 counter = 0
 squares = [None] * 9
@@ -138,7 +136,7 @@ create_bttns()
 index = 0
 for i in range(3):
     for j in range(3):
-        squares[index].place(x=j*100, y=i*100)
+        squares[index].place(x=j*145, y=i*145)
         index += 1
 
 board = [[" ", " ", " "],
